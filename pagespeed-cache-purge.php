39d2d5bdd8ee74649741469f570b719587003dd8
lhansey <lhansey@users.noreply.github.com>
2015-03-21 21:19:05 -0700

    Update pagespeed-cache-purge.php

--- pagespeed-cache-purge.php
+++ pagespeed-cache-purge.php
@@ -9,27 +9,31 @@
  * License: GPL2
  */
 
-add_action('admin_bar_menu', 'pagespeed_dumpnow_adminbar', 200);
+define('pagespeed_purge_url', home_url() . '/pagespeed_admin/cache?purge=*');
+add_action('init', 'pagespeed_init');
 
-if ($_GET['ps_flush_all']) {
-        if (!current_user_can('activate_plugins')) {
-                add_action( 'admin_notice' , 'pagespeed_cache_failed');
-        } else {
-                $resp = wp_remote_get(home_url() . "/pagespeed_admin/cache?purge=*");
-                if ($resp['body'] == 'Purge successful' && $resp['headers']['x-cache'] == 'MISS') {
-                        add_action( 'admin_notices' , 'pagespeed_cache_flushed');
-                } else {
-                        add_action( 'admin_notices' , 'pagespeed_cache_failed');
+function pagespeed_init() {
+        if (current_user_can('activate_plugins')) {
+                add_action('admin_bar_menu', 'pagespeed_dumpnow_adminbar', 200);
+        }
+        if (isset($_GET['ps_flush_all']) && check_admin_referer('ps-cache-purge','_psnonce')) {
+                if (current_user_can('activate_plugins')) {
+                        $resp = wp_remote_get(pagespeed_purge_url);
+                        if ($resp['body'] == 'Purge successful' && $resp['headers']['x-cache'] == "MISS") {
+                                add_action( 'admin_notices' , 'pagespeed_cache_flushed');
+                        } else {
+                                add_action( 'admin_notices' , 'pagespeed_cache_failed');
+                        }
                 }
         }
+
 }
 
 function pagespeed_dumpnow_adminbar($admin_bar){
         $admin_bar->add_menu( array(
                 'id'    => 'purge-pagespeed-cache-all',
                 'title' => 'Purge Pagespeed',
-                                                        /* _wpnonce and vhp_flush_all removal avoids conflict with varnish http purge */
-                'href'  => wp_nonce_url(add_query_arg( array('_wpnonce' => false, 'vhp_flush_all' => false, 'ps_flush_all' => false, 'ps_flush_all' => 1)  ), 'ps-cache-purge', '_psnonce'),
+                'href'  => wp_nonce_url(add_query_arg( array('ps_flush_all' => 1)  ), 'ps-cache-purge', '_psnonce'),
                 'meta'  => array(
                         'title' => __('Purge Pagespeed','ps-cache-purge'),
                 ),
@@ -41,5 +45,6 @@ function pagespeed_cache_flushed() {
 }
 
 function pagespeed_cache_failed() {
-        echo "<div id='message' class='error'><p><strong>".__('Pagespeed cache failed to purge!', 'ps-cache-fail')."</strong></p></div>";
+        echo "<div id='message' class='error'><p><strong>".__('Pagespeed cache failed to purge!', 'ps-cache-purge')."</strong></p></div>";
 }
+
